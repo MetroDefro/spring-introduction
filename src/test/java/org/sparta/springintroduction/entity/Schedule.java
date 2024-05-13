819eb9615dbfc628d586117a4833b043b230e4d3
DESKTOP-UF6GC6B\khu09 <khu4237@gmail.com>
2024-05-14 00:20:22 +0900

    Entity 애너테이션 설정

--- src/test/java/org/sparta/springintroduction/entity/Schedule.java
+++ src/test/java/org/sparta/springintroduction/entity/Schedule.java
@@ -1,10 +1,25 @@
 package org.sparta.springintroduction.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "title", nullable = false, length = 20)
     private String title;
+
+    @Column(name = "contents", nullable = false, length = 255)
     private String contents;
+
+    @Column(name = "charge", nullable = false, length = 10)
     private String charge;
-    private int password;
+
+    @Column(name = "password", nullable = false, length = 20)
+    private String password;
+
+    @Column(name = "date", nullable = false, length = 8)
     private int date;
 }
