68021f5e59ee7d3fe129e95b9b6cea860245679e
DESKTOP-UF6GC6B\khu09 <khu4237@gmail.com>
2024-05-14 12:23:01 +0900

    1단계 - 기능: 일정 작성
    조건
    
    1. `할일 제목`, `할일 내용`, `담당자`, `비밀번호`, `작성일`을 저장할 수 있습니다.
        1. 저장된 일정 정보를 반환 받아 확인할 수 있습니다.

--- src/main/java/org/sparta/springintroduction/controller/ScheduleController.java
+++ src/main/java/org/sparta/springintroduction/controller/ScheduleController.java
@@ -1,14 +1,25 @@
 package org.sparta.springintroduction.controller;
 
+import org.sparta.springintroduction.dto.ScheduleRequestDto;
+import org.sparta.springintroduction.dto.ScheduleResponseDto;
 import org.sparta.springintroduction.service.ScheduleService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/sprig-introduction")
 public class ScheduleController {
 
-    private ScheduleService scheduleService;
+    private final ScheduleService scheduleService;
 
     public ScheduleController(ScheduleService scheduleService) {
         this.scheduleService = scheduleService;
+    }
+
+    @PostMapping("/schedule")
+    public ScheduleResponseDto createSchedule(@RequestBody ScheduleRequestDto scheduleRequestDto) {
+        return scheduleService.createSchedule(scheduleRequestDto);
     }
 }
--- src/main/java/org/sparta/springintroduction/service/ScheduleService.java
+++ src/main/java/org/sparta/springintroduction/service/ScheduleService.java
@@ -1,5 +1,8 @@
 package org.sparta.springintroduction.service;
 
+import org.sparta.springintroduction.dto.ScheduleRequestDto;
+import org.sparta.springintroduction.dto.ScheduleResponseDto;
+import org.sparta.springintroduction.entity.Schedule;
 import org.sparta.springintroduction.repository.ScheduleRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +13,15 @@ public class ScheduleService {
 
     public ScheduleService(ScheduleRepository scheduleRepository) {
         this.scheduleRepository = scheduleRepository;
+    }
+
+    public ScheduleResponseDto createSchedule(ScheduleRequestDto scheduleRequestDto) {
+        Schedule schedule = new Schedule(scheduleRequestDto);
+        Schedule savedSchedule = scheduleRepository.save(schedule);
+        return new ScheduleResponseDto(savedSchedule);
+    }
+
+    private boolean checkPassword(String password) {
+        return true;
     }
 }
