added252368f4b91ceed9b7d5a0acfc0a3999216
DESKTOP-UF6GC6B\khu09 <khu4237@gmail.com>
2024-05-23 16:40:38 +0900

    1️⃣단계 - 일정과 댓글의 연관 관계 댓글 Entity 추가 및 연관 관계 설정
    설명
    
    - 지난 과제에서 만든 일정에 댓글을 추가할 수 있습니다.
    - ERD에도 댓글 모델을 추가합니다.
    - 각 일정에 댓글을 작성할 수 있도록 관련 클래스를 추가하고 연관 관계를 설정합니다.
    - 매핑 관계를 설정합니다. (1:1 or N:1 or N:M)

---
+++ src/main/java/org/sparta/springintroduction/entity/Comment.java
@@ -0,0 +1,37 @@
+package org.sparta.springintroduction.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Entity
+@Table(name = "comment")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "contents", nullable = false, length = 500)
+    private String contents;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+}
